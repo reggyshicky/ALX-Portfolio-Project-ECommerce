66ae7856bb5b214153990de29745d42493ecee98
Reggy Shicky <reginahshikanda@gmail.com>
2024-02-09 01:52:29 +0300

    routing configured

--- Client Side/EcCommerceWeb/src/app/app-routing.module.ts
+++ Client Side/EcCommerceWeb/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
-const routes: Routes = [{ path: 'customer', loadChildren: () => import('./customer/customer.module').then(m => m.CustomerModule) }, { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) }];
+const routes: Routes = [
+  { path : "login", component: LoginComponent },
+  { path : "signup", component: SignupComponent },
+  { path: 'customer', loadChildren: () => import('./customer/customer.module').then(m => m.CustomerModule) },
+  { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
