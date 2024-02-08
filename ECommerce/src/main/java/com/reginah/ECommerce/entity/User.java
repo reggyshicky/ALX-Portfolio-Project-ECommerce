3d5476d4ed2881ad326ce321dc1d7108caad8474
Reggy Shicky <reginahshikanda@gmail.com>
2024-02-08 03:19:13 +0300

    linked mariadb to the spring application successfully

---
+++ ECommerce/src/main/java/com/reginah/ECommerce/entity/User.java
@@ -0,0 +1,21 @@
+package com.reginah.ECommerce.entity;
+
+import com.reginah.ECommerce.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String password;
+    private String name;
+    private UserRole role;
+    @Lob
+    @Column(columnDefinition = "longblob")
+    private byte[] image;
+}
---
+++ ECommerce/src/main/java/com/reginah/ECommerce/enums/UserRole.java
@@ -0,0 +1,6 @@
+package com.reginah.ECommerce.enums;
+
+public enum UserRole {
+    ADMIN,
+    CUSTOMER
+}
