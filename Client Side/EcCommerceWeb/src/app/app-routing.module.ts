46f5d0ee0fc3758cb00290ae22be1857e89da5ec
Reggy Shicky <reginahshikanda@gmail.com>
2024-02-09 01:27:49 +0300

    admin component/module created - Angular

--- Client Side/EcCommerceWeb/src/app/app-routing.module.ts
+++ Client Side/EcCommerceWeb/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'customer', loadChildren: () => import('./customer/customer.module').then(m => m.CustomerModule) }];
+const routes: Routes = [{ path: 'customer', loadChildren: () => import('./customer/customer.module').then(m => m.CustomerModule) }, { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
