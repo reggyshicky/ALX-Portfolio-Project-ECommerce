03ee27a69af56439e159bb81854a6ef68d18c610
Reggy Shicky <reginahshikanda@gmail.com>
2024-02-08 10:59:21 +0300

    created an angulsr project

---
+++ EcCommerceWeb/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { DemoAngularMaterailModule } from './DemoAngularMaterialModule';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    DemoAngularMaterailModule
+  ],
+  providers: [
+    provideClientHydration(),
+    provideAnimationsAsync()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
